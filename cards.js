a4dc354402cd38773695f70d8579974a222d4f96
wanhsuanlee <awanwan940204@gmail.com>
2021-11-04 16:47:41 +0800

    完成紙牌外觀，且透過 JS 複製 52 張四種花色的紙牌

---
+++ cards.js
@@ -0,0 +1,44 @@
+let cards = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+    32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+];
+let revealedCards = [1, 2];
+let score = 0;
+const Symbols = [
+    'https://cdn-icons-png.flaticon.com/512/1/1438.png', // 黑桃
+    'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
+    'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
+    'https://cdn-icons-png.flaticon.com/512/220/220757.png', // 梅花
+];
+const view = {
+    getCardElement(index) {
+        const number = this.transformNumber((index % 13) + 1);
+        const symbol = Symbols[Math.floor(index / 13)];
+        return `<div class="card">
+        <p>${number}</p>
+        <img src="${symbol}" />
+        <p>${number}</p></div>`;
+    },
+    transformNumber(number) {
+        switch (number) {
+            case 1:
+                return 'A';
+            case 11:
+                return 'J';
+            case 12:
+                return 'Q';
+            case 13:
+                return 'K';
+            default:
+                return number;
+        }
+    },
+
+    displayCards() {
+        const rootElement = document.querySelector('#cards');
+        rootElement.innerHTML = Array.from(Array(52).keys())
+            .map((index) => this.getCardElement(index))
+            .join('');
+    },
+};
+view.displayCards();
